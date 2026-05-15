0531b591315447e491755306d837fcadd6e3c74a
Entar <entar57mailru@mail.ru>
2026-05-15 07:37:15 +0700

    Fixed up namings

--- pawtrix/ViewModels/LoginWindowViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace pawtrix.ViewModels;
-
-public class LoginWindowViewModel : ViewModelBase
-{
-    public string Greeting { get; } = "Welcome to Pawtrix! \nPut in your homeserver, login and password below.";
-    public string Homeserver { get; set; } = "";
-    public string Login { get; set; } = "";
-    public string Password { get; set; } = "";
-}
--- pawtrix/ViewModels/ViewModelBase.cs
+++
@@ -1,7 +0,0 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
-
-namespace pawtrix.ViewModels;
-
-public abstract class ViewModelBase : ObservableObject
-{
-}
